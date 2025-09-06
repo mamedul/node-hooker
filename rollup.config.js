52249c5c7e1dda6f7dd1f33f88e99ffc90b85090
mamedul <md.mamedulislam@gmail.com>
2025-09-07 01:41:53 +0600

    2025.09.07.01

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 // Check if the NODE_ENV is 'production'
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
-  input: 'index.js',
+  input: './lib/Hooker.js',
   output: {
     // Dynamically set the filename
-    file: isProduction ? './dist/node-hooker.umd.min.js' : './dist/node-hooker.umd.js',
+    file: isProduction ? 'dist/node-hooker.umd.min.js' : 'dist/node-hooker.umd.js',
     format: 'umd',
     name: 'Hooker', 
   },
